d3473ecd62724e12ee9a710ce0361a5483f1ee26
KozenUmezawa <umezawa@viz.media.kyoto-u.ac.jp>
2016-06-14 20:55:12 +0900

    Realize drawing prisms.

--- src/DataFile.js
+++ src/DataFile.js
@@ -1,11 +1,25 @@
 function DataFile(id) {
   this.id = id;
+  this.dataFlag = false;
 }
 
 DataFile.prototype.setData = function(data) {
   this.data = data;
+  this.dataFlag = true;
 }
 
+DataFile.prototype.setFileType = function(type) {
+  this.type = type;
+}
+
+DataFile.prototype.getCoord = function(idx) { 
+  var result = [];
+  result[0] = this.data[idx * 3];
+  result[1] = this.data[idx * 3 + 1];
+  result[2] = this.data[idx * 3 + 2];
+  
+  return result;
+}
 DataFile.prototype.getData = function() {
   var binFile = document.getElementById(this.id).files;
   if(!binFile) return;
@@ -17,7 +31,17 @@ DataFile.prototype.getData = function() {
   var self = this;
   //ファイルの読み込み成功時
   fileReader.onload = function(e) {
-    self.setData(new Uint32Array(fileReader.result));
+    switch(self.type){
+      case 'Uint':
+        self.setData(new Uint32Array(fileReader.result));
+        break;
+      case 'Float':
+        self.setData(new Float32Array(fileReader.result));
+        break;
+      default:
+        break;
+    }
   }
+
 }
 
--- src/main.js
+++ src/main.js
@@ -1,46 +1,72 @@
+//calculateボタンが押されたときの処理
 function getBinFile() {
   var coordData = new DataFile('coordFiles');
   var valueData = new DataFile('valueFiles');
   var connectData = new DataFile('connectFiles');
 
+  coordData.setFileType('Float');
+  valueData.setFileType('Float');
+  connectData.setFileType('Uint');
+  
   coordData.getData();
   valueData.getData();
   connectData.getData();
+
+  var timer;
+  var checkData = function(){
+    if(coordData.dataFlag == true && valueData.dataFlag == true && connectData.dataFlag == true){
+      //データ所得後の処理
+      console.log('start drawing');
+      for(var t = 0; t < 20; t = t + 6){
+        var vv0 = coordData.getCoord(connectData.data[t + 0]);
+        var vv1 = coordData.getCoord(connectData.data[t + 1]);
+        var vv2 = coordData.getCoord(connectData.data[t + 2]);
+        var vv3 = coordData.getCoord(connectData.data[t + 3]);
+        var vv4 = coordData.getCoord(connectData.data[t + 4]);
+        var vv5 = coordData.getCoord(connectData.data[t + 5]);
+        drawPrism(vv0, vv1, vv2, vv3, vv4, vv5);
+      }
+      camera.position.x = coordData.data[0];
+      camera.position.y = coordData.data[1];
+      camera.position.z = coordData.data[2];
+      console.log('finish drawing');
+      clearInterval(timer);
+    }
+  }
+
+  timer = setInterval(checkData, 500);  //500ミリ秒毎に読み込みが終わったか確認
 }
 
-
-
-(function() {
-
-
-  var scene = new THREE.Scene();
-  var camera = new THREE.PerspectiveCamera(45, 1, 1, 1000);
-  var controls = new THREE.OrbitControls(camera);
-
-  camera.position.z = 70;
-
-  var renderer = new THREE.WebGLRenderer();
-  renderer.setSize(400, 400);
-
-  document.getElementById('output').appendChild(renderer.domElement);
-  var directionalLight = new THREE.DirectionalLight(0xffffff);
-  directionalLight.position.set(0, 50, 50);
-  scene.add(directionalLight);
-
+function drawPrism(v0, v1, v2, v3, v4, v5){
   var geometry = new THREE.Geometry();
-  geometry.vertices.push(new THREE.Vector3(0, 0, 0));
-  geometry.vertices.push(new THREE.Vector3(-160, 50, 30));
-  geometry.vertices.push(new THREE.Vector3(-160, 80, 100));
 
+  geometry.vertices.push(new THREE.Vector3(v0[0], v0[1], v0[2]));
+  geometry.vertices.push(new THREE.Vector3(v1[0], v1[1], v1[2]));
+  geometry.vertices.push(new THREE.Vector3(v2[0], v2[1], v2[2]));
+  geometry.vertices.push(new THREE.Vector3(v3[0], v3[1], v3[2]));
+  geometry.vertices.push(new THREE.Vector3(v4[0], v4[1], v4[2]));
+  geometry.vertices.push(new THREE.Vector3(v5[0], v5[1], v5[2]));
   var material = new THREE.LineBasicMaterial({color: 0xFFFFFF});
   var line = new THREE.Line(geometry, material);
   scene.add(line);
+}
 
-  render();
 
-  function render() {
-    requestAnimationFrame(render);
-    controls.update();
-    renderer.render(scene, camera);
-  }
-}())
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(45, 1, 0.1, 1000);
+var controls = new THREE.OrbitControls(camera);
+
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(400, 400);
+
+document.getElementById('output').appendChild(renderer.domElement);
+
+render();
+
+
+function render() {
+  requestAnimationFrame(render);
+  controls.update();
+  renderer.render(scene, camera);
+}
+

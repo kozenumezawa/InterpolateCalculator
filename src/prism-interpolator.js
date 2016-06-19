63afda7f05b5b52fab50fc732b77b0429cba6c13
KozenUmezawa <umezawa@viz.media.kyoto-u.ac.jp>
2016-06-19 12:42:36 +0900

    Add a function of prism's volume calculation.

--- src/prism-interpolator.js
+++ src/prism-interpolator.js
@@ -1,4 +1,4 @@
-// prism's verticies are defined as follows:
+// prism's vertices are defined as follows:
 //        N2          N0=(0,0,1)  (=(p,q,r))
 //       / \          N1=(1,0,1)
 //      /  \          N2=(0,1,1)
@@ -6,16 +6,16 @@
 //    |     |         N4=(1,0,0)
 //    |     |         N5=(0,1,0)
 //    N3----N4
-
+//
+//So constructor have to check vertex's positional relation
+//and exchange vertices number if it is wrong. (Incomplete)
 
 class prismCell {
   constructor(v0, v1, v2, v3, v4, v5, s0, s1, s2, s3, s4, s5) {
-    let V = [v0, v1, v2, v3, v4, v5];
-    let S = [s0, s1, s2, s3, s4, s5];
-    
     this.V = [ v0, v1, v2, v3, v4, v5 ];  //coordinates
     this.S = [ s0, s1, s2, s3, s4, s5 ];  //scalar
-    this.verticies = 6;
+    this.vertices = 6;
+    this.volume = this.calculateVolume();
   }
 
   localToGlobal(local) {
@@ -25,7 +25,7 @@ class prismCell {
 
     const N = this.getInterpolationFunctions(local);
 
-    for (let i = 0; i < this.verticies; i++) {
+    for (let i = 0; i < this.vertices; i++) {
       x += N[ i ] * this.V[ i ][ 0 ];
       y += N[ i ] * this.V[ i ][ 1 ];
       z += N[ i ] * this.V[ i ][ 2 ];
@@ -66,7 +66,7 @@ class prismCell {
     const q = local[1];
     const r = local[2];
 
-    let N = new Array(this.verticies);
+    let N = new Array(this.vertices);
 
     N[ 0 ] = (1 - p - q) * r;
     N[ 1 ] = p * r;
@@ -78,14 +78,54 @@ class prismCell {
     return N;
   }
 
+  interpolateScalar(local) {
+    let N = this.getInterpolationFunctions(local);
+
+    let S = 0;
+    for(let i = 0; i < this.vertices; i++){
+      S += N[i] * this.S[i];
+    }
+    return S;
+  }
+  
+  randomSampling() {
+    let p = Math.random();
+    let q = Math.random();
+    let r = Math.random();
+    let local = new Array(3);
+    
+    if(p + q > 1) {
+      local[0] = 1 - p;
+      local[1] = 1 - q;
+      local[2] = r;
+    }else {
+      local[0] = p;
+      local[1] = q;
+      local[2] = r;
+    }
+    return local;
+  }
+
+  calculateVolume() {
+    //Requirement: vertex V[0], V[1], V[2] configure a triangle and distance of V[0] and V[3] denotes height.
+    let a = [this.V[1][0] - this.V[0][0], this.V[1][1] - this.V[0][1], this.V[1][2] - this.V[0][2]];
+    let b = [this.V[2][0] - this.V[0][0], this.V[2][1] - this.V[0][1], this.V[2][2] - this.V[0][2]];
+
+    let vectorProduct = [a[1] * b[2] - a[2] * b[1],
+                         a[2] * b[0] - a[0] * b[2],
+                         a[0] * b[1] - a[1] * b[0]];
+    let S1 = Math.sqrt(Math.pow(vectorProduct[0], 2) + Math.pow(vectorProduct[1], 2) + Math.pow(vectorProduct[2], 2))* 0.5;
+    return S1 * this.distance(this.V[0], this.V[3]);
+  }
+
   differentialFunction(local) {
     const p = local[0];
     const q = local[1];
     const r = local[2];
 
-    let dNdp = new Array(this.verticies);
-    let dNdq = new Array(this.verticies);
-    let dNdr = new Array(this.verticies);
+    let dNdp = new Array(this.vertices);
+    let dNdq = new Array(this.vertices);
+    let dNdr = new Array(this.vertices);
 
     dNdp[0] = -r;
     dNdp[1] =  r;
@@ -118,7 +158,7 @@ class prismCell {
     let dxdp = 0;
     let dydp = 0;
     let dzdp = 0;
-    for(let i = 0; i < this.verticies; i++){
+    for(let i = 0; i < this.vertices; i++){
       dxdp += dN[0][i] * this.V[i][0];
       dydp += dN[0][i] * this.V[i][1];
       dzdp += dN[0][i] * this.V[i][2];
@@ -127,7 +167,7 @@ class prismCell {
     let dxdq = 0;
     let dydq = 0;
     let dzdq = 0;
-    for(let i = 0; i < this.verticies; i++){
+    for(let i = 0; i < this.vertices; i++){
       dxdq += dN[1][i] * this.V[i][0];
       dydq += dN[1][i] * this.V[i][1];
       dzdq += dN[1][i] * this.V[i][2];
@@ -136,7 +176,7 @@ class prismCell {
     let dxdr = 0;
     let dydr = 0;
     let dzdr = 0;
-    for(let i = 0; i < this.verticies; i++){
+    for(let i = 0; i < this.vertices; i++){
       dxdr += dN[2][i] * this.V[i][0];
       dydr += dN[2][i] * this.V[i][1];
       dzdr += dN[2][i] * this.V[i][2];
@@ -145,35 +185,13 @@ class prismCell {
     const jacobiMatrix = [[dxdp, dydp, dzdp], [dxdq, dydq, dzdq], [dxdr, dydr, dzdr]];
     return jacobiMatrix;
   }
-
-  interpolateScalar(local) {
-    let N = this.getInterpolationFunctions(local);
-
-    let S = 0;
-    for(let i = 0; i < this.verticies; i++){
-      S += N[i] * this.S[i];
-    }
-    return S;
-  }
   
-  randomSampling() {
-    let p = Math.random();
-    let q = Math.random();
-    let r = Math.random();
-    let local = new Array(3);
-    
-    if(p + q > 1) {
-      local[0] = 1 - p;
-      local[1] = 1 - q;
-      local[2] = r;
-    }else {
-      local[0] = p;
-      local[1] = q;
-      local[2] = r;
-    }
-    return local;
+  distance(A, B) {
+    let r;
+    r = Math.sqrt(Math.pow(A[0] - B[0], 2) + Math.pow(A[1] - B[1], 2) + Math.pow(A[2] - B[2], 2));
+    return r;
   }
-  
+
   determinant(A) {
     const a0 = A[0][0] * A[1][1] * A[2][2];
     const a1 = A[1][0] * A[2][1] * A[0][2];
@@ -238,12 +256,13 @@ class prismCell {
     return C;
   }
 
-  minus_1_1(A, B){
+  minus_1_1(A, B) {
     let C = new Array(3);
     C[0] = A[0] - B[0];
     C[1] = A[1] - B[1];
     C[2] = A[2] - B[2];
     return C;
   }
+
 }
 
